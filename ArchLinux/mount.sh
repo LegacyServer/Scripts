0773c64abbaa5ec33e1fe6724403b9bdd13cc837
Deepak Sharma <TechExhibeo@gmail.com>
2016-01-25 17:26:39 +0530

    add ArchLinux scripts

---
+++ ArchLinux/mount.sh
@@ -0,0 +1,37 @@
+images=(
+        # img mount_point
+	'deepak.ext4 /home/deepak deepak'
+	'vatsal.ext4 /home/vatsal vatsal'
+        'sud.ext4 /home/sud sud'
+        'fenix.ext4 /home/fenix fenix'
+        'legacy.ext4 /home/legacy legacy'
+        'vivek.ext4 /home/vivek vivek'
+        'saatvik.ext4 /home/saatvik saatvik'
+        'soham.ext4 /home/soham soham'
+        'shivam.ext4 /home/shivam shivam'
+        'sanyam.ext4 /home/sanyam sanyam'
+	'ccache.ext4 /home/ccache'
+	'common.ext4 /home/common common'
+)
+
+
+count=0
+
+while [ "x${images[count]}" != "x" ]; do
+        curr="${images[count]}"
+        image=`echo "$curr" | awk '{print $1}'`
+        mount=`echo "$curr" | awk '{print $2}'`
+        user=`echo "$curr" | awk '{print $3}'`
+
+        if [ ! "$1" == "umount" ]; then
+                mount -t ext4 -o loop /home/images/$image $mount
+                chown "$user":builders $mount
+        else
+                umount $mount
+        fi
+
+        count=$(( $count + 1 ))
+done
+
+usermod -a -G builders jenkins
+chown jenkins:builders /home/ccache
