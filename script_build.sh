455fde9c55e49d9b55c3ee35546af095d79f1c06
Deepak Sharma <TechExhibeo@gmail.com>
2019-09-09 23:03:42 +0530

    Use system installed ccache
    Android 10 removes build ccache prebuilts.
    Add installclean parameter
    Cleanup few things

--- script_build.sh
+++ script_build.sh
@@ -11,14 +11,6 @@
 # Replace "legacy" with your own SSH Username in lowercase
 username=legacy
 
-# Assign values to parameters used in Script from Jenkins Job parameters
-use_ccache="$1"
-make_clean="$2"
-lunch_command="$3"
-device_codename="$4"
-target_command="$5"
-build_type="$6"
-
 # Colors makes things beautiful
 export TERM=xterm
 
@@ -33,17 +25,19 @@ export TERM=xterm
 if [ "$use_ccache" = "yes" ];
 then
 echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_DIR=/home/ccache/$username
-prebuilts/misc/linux-x86/ccache/ccache -M 50G
+ccache -M 50G
 fi
 
 if [ "$use_ccache" = "clean" ];
 then
+export CCACHE_EXEC=$(which ccache)
 export CCACHE_DIR=/home/ccache/$username
 ccache -C
 export USE_CCACHE=1
-prebuilts/misc/linux-x86/ccache/ccache -M 50G
+ccache -M 50G
 wait
 echo -e ${grn}"CCACHE Cleared"${txtrst};
 fi
@@ -56,7 +50,15 @@ wait
 echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
 fi
 
+# Its Images Clean Time
+if [ "$make_clean" = "installclean" ];
+then
+make installclean
+wait
+echo -e ${cya}"Images deleted from OUT dir"${txtrst};
+fi
+
 # Build ROM
-. build/envsetup.sh
+source build/envsetup.sh
 lunch "$lunch_command"_"$device_codename"-"$build_type"
-make "$target_command" -j8
+make "$target_command" -j"$jobs"
